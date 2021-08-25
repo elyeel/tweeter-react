ddf098f7a9bc9e3548859bd7000d643e68aefe94
Risyardi Iswanto <elyeel@yahoo.com>
2021-08-24 23:18:58 -0700

    first commit, created 4 components in components folder as per 'creating our components' task

---
+++ react-version/src/components/TweetForm.js
@@ -0,0 +1,3 @@
+export default function TweetForm() {
+	return <h1>I am the tweet form component</h1>;
+}
---
+++ react-version/src/components/TweetList.js
@@ -0,0 +1,3 @@
+export default function TweetList() {
+	return <h1>I am the tweet list component</h1>;
+}
